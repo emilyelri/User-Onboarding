65146bf693e0a86f6d77f101f312c1a9af53fec8
Emily_Richard <richard.emily.e@gmail.com>
2019-09-19 23:25:38 -0400

    Finished updates

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Form from './components/Form';
 import Footer from './components/Footer';
-import './css/index.css';
 
 function App() {
   return (
--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -2,7 +2,6 @@ import React, {useState, useEffect} from "react";
 import { withFormik, Form, Field } from "formik";
 import axios from "axios";
 import * as Yup from "yup";
-import '../css/index.css';
 
 function LoginForm( {values, errors, touched, status} ) {
     const [users, setUsers] = useState([]);
